2c66f2920dde843171743c7f8cbfb7fc462a5396
Swearfy <swearfy@gmail.com>
2022-02-10 12:36:11 +0000

    Added testing for stock class

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStock AnStock = new clsStock();
+            Assert.IsNotNull(AnStock);
+            
+        }
+    }
+}
