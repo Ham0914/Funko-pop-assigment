97a1f331757958e7868bc55b349d6102abb988c4
Jeev Pangli <jspanglez@gmail.com>
2022-02-15 16:48:04 +0000

    Added more tests for the staff class

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public int staffID { get; set; }
+
+        public bool Active { get; set; }
+        public DateTime HireDate { get; set; }
+        public string staffEmail { get; set; }
+        public string staffName { get; set; }
+        public string department { get; set; }
     }
 }
