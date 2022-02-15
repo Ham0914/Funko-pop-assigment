fc5d1c79e684e5025df3ff5a05f5fdbff2253a4d
Abdul Morad <P2590746@my365.dmu.ac.uk>
2022-02-15 14:21:51 +0000

    Added attribute testing

--- Testing1/tstOrder.cs
+++ Testing1/tstOrder.cs
@@ -14,7 +14,78 @@ namespace Testing1
             clsOrder AnOrder = new clsOrder();
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
-
         }
+        [TestMethod]
+        public void AvailablePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnOrder.Available = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Available, TestData);
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnOrder.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void OrderNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.OrderNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderNo, TestData);
+        }
+        public void FunkoNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            string TestData = "Hulk";
+            //assign the data to the property
+            AnOrder.FunkoName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.FunkoName, TestData);
+        }
+        [TestMethod]
+        public void FunkoNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.FunkoNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.FunkoNo, TestData);
+        }
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            double TestData = 9.99;
+            //assign the data to the property
+            AnOrder.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Price, TestData);
+        }
+
     }
 }
