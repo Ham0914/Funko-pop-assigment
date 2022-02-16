5936b5f404cb168b049fc6701e89484ce0f8dada
Abdul Morad <P2590746@my365.dmu.ac.uk>
2022-02-16 22:19:17 +0000

    created interface for order

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using ClassLibrary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture the order number
+        AnOrder.FunkoName = txtFunkoName.Text;
+        //store the address in the session object
+        Session["AnOrder"] = AnOrder;
+        //navigate to the viewer page
+        Response.Redirect("OrderViewer.aspx");
     }
 }
--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using ClassLibrary;
 using System.Web.UI.WebControls;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new insatnce of clsOrder
+        clsOrder anOrder = new clsOrder();
+        //get the data from the session object
+        anOrder = (clsOrder)Session["AnOrder"];
+        //display the order number for this entry
+        Response.Write(anOrder.FunkoName);
     }
 }
