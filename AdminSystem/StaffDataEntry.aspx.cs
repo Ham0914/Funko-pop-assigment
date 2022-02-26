428c0f21e6daf10ab4350b53d1c1fa1283665332
Jeev Pangli <jspanglez@gmail.com>
2022-02-26 00:03:04 +0000

    Created labels and buttons for Staff.

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //Create a new instance of clsStaff
+        clsStaff aStaff = new clsStaff
+        {
+            staffID = Convert.ToInt32(txtStaffID.Text),
+            staffEmail = txtStaffEmail.Text,
+            staffName = txtStaffName.Text,
+            department = txtDepartment.Text,
+            HireDate = Convert.ToDateTime(txtHireDate.Text),
+            Active = Convert.ToBoolean(chkActive.Checked)
+        };
+
+        Session["aStaff"] = aStaff;
+
+        Response.Redirect("StaffViewer.aspx");
     }
 }
