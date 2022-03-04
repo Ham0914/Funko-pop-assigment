ba86c2448d5621ffb9a188f911b7195aef0dfa77
Jeev Pangli <jspanglez@gmail.com>
2022-03-04 13:08:23 +0000

    Added the Find button.

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -30,4 +30,27 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
         Response.Redirect("StaffViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create an instance of the Staff class
+        clsStaff aStaff = new clsStaff();
+        //variable to store the primary key
+        Int32 staffID;
+        //variable to store the result of the find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        staffID = Convert.ToInt32(txtStaffID.Text);
+        //find the record
+        Found = aStaff.Find(staffID);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtStaffEmail.Text = aStaff.staffEmail;
+            txtStaffName.Text = aStaff.staffName;
+            txtDepartment.Text = aStaff.department;
+            txtHireDate.Text = aStaff.HireDate.ToString();
+        }
+    }
 }
