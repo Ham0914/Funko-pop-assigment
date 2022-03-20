82d474894ecedf03e26ebde07b8cef29c7a6b707
Abdul Morad <P2590746@my365.dmu.ac.uk>
2022-03-20 14:40:25 +0000

    sfdj

--- AdminSystem/OrderList.aspx.cs
+++ AdminSystem/OrderList.aspx.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using ClassLibrary;
 using System.Web.UI.WebControls;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayOrders();
+        }
+    }
 
+    void DisplayOrders()
+    {
+        clsOrderCollection Orders = new clsOrderCollection();
+        lstOrderList.DataSource = Orders.OrderList;
+        lstOrderList.DataValueField = "OrderNo";
+        lstOrderList.DataTextField = "PostCode";
+        lstOrderList.DataBind();
     }
 }
