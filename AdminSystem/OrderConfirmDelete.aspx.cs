82efec0064b8a334aa54a6b07539f8dbc51a083d
Abdul Morad <P2590746@my365.dmu.ac.uk>
2022-03-23 12:27:41 +0000

    did third part of week 27

--- AdminSystem/OrderConfirmDelete.aspx.cs
+++ AdminSystem/OrderConfirmDelete.aspx.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using ClassLibrary;
 using System.Web.UI.WebControls;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 FunkoNo;
     protected void Page_Load(object sender, EventArgs e)
     {
+        FunkoNo = Convert.ToInt32(Session["FunkoNo"]);
+    }
 
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        clsOrderCollection Order = new clsOrderCollection();
+        Order.ThisOrder.Find(FunkoNo);
+        Order.Delete();
+        Response.Redirect("OrderList.aspx");
     }
 }
